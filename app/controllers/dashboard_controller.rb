b4cd32a65186578d5343c2f70034ca2683d7e59d
Ukesh <godsfear@gmail.com>
2018-09-01 04:57:56 -0700

    added new smileys

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
+    @today = Submission.today
+    @good_counts = Answer.group(:answer_text).count["good"]
+    @bad_counts = Answer.group(:answer_text).count["bad"]
   end
   
 end
