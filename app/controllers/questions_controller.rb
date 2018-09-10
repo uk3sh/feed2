fd739308d44b23ad1089dbbe74649e04bf08017f
Ukesh <godsfear@gmail.com>
2018-09-10 07:03:07 -0700

    added star rating

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -81,6 +81,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:question_text, :option => [])
+      params.require(:question).permit(:question_text, :question_type, :option => [])
     end
 end
