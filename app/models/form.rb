a49b836b119c8c5b06813b77f7d8fb3c170d6d6a
Ukesh <godsfear@gmail.com>
2018-09-14 10:07:12 -0700

    added fonts, fixed modal css, added pagination css

--- app/models/form.rb
+++ app/models/form.rb
@@ -4,14 +4,26 @@ class Form < ApplicationRecord
   validates :sender_ID, presence: true, length: { is: 6 }
   validates :message, presence: true
 
+  
+
+  
+
   has_many :questions
   has_many :submissions 
 
   def generate_url(subdomain, domain)
+
+    chars = ['0'..'9', 'A'..'Z', 'a'..'z'].map { |range| range.to_a }.flatten
+    short_url = 6.times.map { chars.sample }.join
+    puts short_url
+    slug = 'http://' + subdomain + '.' + domain + '/'+ short_url
+    puts slug
+
     x = 'http://' + subdomain + '.' + domain + '/' + 'forms/' + self.id.to_s + '/' + 'submissions/new'
-    self.url = x    
+    self.url = x   
+     
   end
   
-
+  
   
 end
