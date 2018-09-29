54090d6b255d22b0f23484ba1977a5c524ea2c1c
Ukesh <godsfear@gmail.com>
2018-09-28 22:46:15 -0700

    edited registrations controller-1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
-  def after_sign_up_path_for(resource)
-    new_user_session_url(subdomain: resource.subdomain)
-  end
+  
 end
